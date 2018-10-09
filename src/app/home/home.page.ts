8ef1b10eae0865d5e077ef91a41808237aabd4ee
Luis José Sánchez <luisjose@espiral.io>
2018-10-09 23:19:31 +0200

    Added form, ngOnInit and other changes.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { DataService } from '../services/data.service';
+import { RedditService } from '../services/reddit.service';
+import { ModalController } from '@ionic/angular';
+
+// Estas importaciones se usan más tarde
+import { Plugins } from '@capacitor/core';
+import { SettingsPage } from '../settings/settings.page';
+import { debounceTime } from 'rxjs/operators';
+import { distinctUntilChanged } from 'rxjs/operators';
+const { Browser, Keyboard } = Plugins;
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
+  public subredditForm: FormGroup;
+
+  constructor(
+    private dataService: DataService,
+    public redditService: RedditService,
+    private modalController: ModalController) {
+
+      this.subredditForm = new FormGroup({
+        subredditControl: new FormControl('')
+      });
+  }
+
+  ngOnInit() {
+    this.redditService.load();
+
+    this.subredditForm.get('subredditControl').valueChanges.pipe(
+      debounceTime(1500),
+      distinctUntilChanged(),
+    ).subscribe((subreddit: any) => {
+      if (subreddit.length > 0) {
+        this.redditService.changeSubreddit(subreddit);
+        Keyboard.hide().catch((err) => {
+          console.warn(err);
+        });
+      }
+    });
+  }
+
+  showComments(post): void { }
+
+  openSettings(): void { }
+
+  playVideo(e, post): void { }
+
+  loadMore(): void { }
 }
